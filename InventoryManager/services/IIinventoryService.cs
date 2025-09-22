5a3e27c571dcfaf86986ae66332448174de0ff0c
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2025-09-22 19:27:28 -0300

    feat: Add InventoryService and IInventoryService

---
+++ InventoryManager/services/IIinventoryService.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManager.models;
+
+namespace InventoryManager.services
+{
+    public interface IIinventoryService
+    {
+        public void AddProduct(Product product);
+        
+
+        public Product? GetProductByName(string name);
+        
+
+        public void DeleteProductByName(string name);
+        public void UpdateProduct(string name, decimal newPrice,int newAmount);
+
+        public List<Product> GetProducts();
+        
+        
+    }
+}
---
+++ InventoryManager/services/InventoryService.cs
@@ -0,0 +1,67 @@
+﻿using InventoryManager.models;
+
+namespace InventoryManager.services
+{
+    public class InventoryService : IIinventoryService
+    {
+        private List<Product> productsList;
+
+        public InventoryService()
+        {
+            productsList = new List<Product>();
+        }
+
+
+        public Product? GetProductByName(string name)
+        {
+            foreach (var product in productsList)
+            {
+                if (product.Name == name)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return productsList;
+        }
+
+
+        public void AddProduct(Product product)
+        {
+            productsList.Add(product);
+        }
+
+        public void UpdateProduct(string name, decimal newPrice, int newAmount)
+        {
+            var searchProduct = GetProductByName(name);
+            if (searchProduct != null)
+            {
+                searchProduct.Price = newPrice;
+                searchProduct.Amount = newAmount;
+            }
+            else
+            {
+                Console.WriteLine("Product not found.");
+            }
+        }
+
+        public void DeleteProductByName(string name)
+        {
+            var deleteProduct = GetProductByName(name);
+            if (deleteProduct != null)
+            {
+                productsList.Remove(deleteProduct);
+            }
+            else
+            {
+                Console.WriteLine("Product not found");
+            }
+        }
+
+
+    }
+}
