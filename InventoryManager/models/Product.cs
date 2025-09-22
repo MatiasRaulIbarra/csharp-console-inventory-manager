551b135c7a1964e06e4530987d41cdda70124030
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2025-09-22 18:12:51 -0300

    feat: Add Product class

---
+++ InventoryManager/models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace InventoryManager.models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+
+        public Product(string name, decimal price, int amount)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Amount = amount;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
